2a2a389a144dccad9eab392c4acabe816a68e7ae
Scobber <scott@pep.id.au>
2021-12-21 08:42:18 +1100

    Update installdeps.sh

--- dep/installdeps.sh
+++ dep/installdeps.sh
@@ -6,6 +6,6 @@ apt clean && rm -R /var/lib/apt/lists/*
 cd /opt
 git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/scripts
-./build_deps.sh && cd ../build
+bash ./build_deps.sh && cd ../build
 cmake .. -DXMRIG_DEPS=scripts/deps
 make -j$(nproc)
