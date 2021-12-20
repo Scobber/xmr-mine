4b139036f92a34d8ef97f8add0c9a9f733a8fd45
Scobber <scott@pep.id.au>
2021-12-21 09:52:55 +1100

    add dep

--- obj/build.sh
+++ obj/build.sh
@@ -2,6 +2,10 @@
 cd /opt
 git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/scripts
+ls
+chmod +x build.uv.sh
+chmod +x build.hwloc.sh
+chmod +x build.openssl.sh
 bash ./build_deps.sh && cd ../build
 cmake .. -DXMRIG_DEPS=scripts/deps
 #make -j$(nproc)
